384cb0f56c9936ac06e2663562d294d163fd42ac
Alessandro Briosi <ab1@metalit.com>
2017-01-18 12:31:14 +0100

    SQL Server migrations fixes and improvements

--- migrations/Migration.php
+++ migrations/Migration.php
@@ -55,6 +55,25 @@ class Migration extends \yii\db\Migration
         }
     }
     
+    public function dropTable($table)
+    {
+        if ($this->dbType = 'sqlsrv') {
+            $this->dropTableConstraints($table);
+        }
+        return parent::dropTable($table);
+    }
+    
+    public function dropColumn($table, $column)
+    {
+        if ($this->dbType = 'sqlsrv') {
+            $this->dropColumnConstraints($table, $column);
+        }
+        return parent::dropColumn($table, $column);
+    }
+    
+    /*
+     *  Drops contratints and Indexes referencind a Table Column
+     */
     public function dropColumnConstraints($table, $column)
     {
         $table = Yii::$app->db->schema->getRawTableName($table);
@@ -64,12 +83,44 @@ class Migration extends \yii\db\Migration
                                     SELECT column_id 
                                     FROM sys.columns 
                                     WHERE object_id = object_id(:table)
-                                    and name = :column
+                                          AND name = :column
                                 )', [ ':table' => $table, ':column' => $column ]);
                                 
         $constraints = $cmd->queryAll();
         foreach ($constraints as $c) {
             $this->execute('ALTER TABLE '.Yii::$app->db->quoteTableName($table).' DROP CONSTRAINT '.Yii::$app->db->quoteColumnName($c['name']));
         }
+        
+        // checking for indexes
+        $cmd = Yii::$app->db->createCommand('SELECT ind.name FROM sys.indexes ind
+                                                INNER JOIN sys.index_columns ic 
+                                                    ON  ind.object_id = ic.object_id and ind.index_id = ic.index_id 
+                                                INNER JOIN sys.columns col 
+                                                    ON ic.object_id = col.object_id and ic.column_id = col.column_id 
+                                                WHERE ind.object_id = object_id(:table)
+                                                AND col.name = :column',
+                                [ ':table' => $table, ':column' => $column ]);
+                                
+        $indexes = $cmd->queryAll();
+        foreach ($indexes as $i) {
+            $this->dropIndex($i['name'],$table);
+        }
     }
+    
+    /*
+     *  Drops contratints referencing the Table
+     */
+    public function dropTableConstraints($table)
+    {
+        $table = Yii::$app->db->schema->getRawTableName($table);
+        $cmd = Yii::$app->db->createCommand('SELECT name, OBJECT_NAME(parent_object_id) as tbl FROM sys.foreign_keys
+                                WHERE referenced_object_id = object_id(:table)',
+                                [ ':table' => $table ]);
+        $constraints = $cmd->queryAll();
+        foreach ($constraints as $c) {
+            echo 'Dropping constrain: '.$c['name']."\n";
+            $this->execute('ALTER TABLE '.Yii::$app->db->quoteTableName($c['tbl']).' DROP CONSTRAINT '.Yii::$app->db->quoteColumnName($c['name']));
+        }
+    }
+    
 }
--- migrations/m140830_171933_fix_ip_field.php
+++ migrations/m140830_171933_fix_ip_field.php
@@ -18,6 +18,10 @@ class m140830_171933_fix_ip_field extends Migration
 {
     public function up()
     {
+        if ($this->dbType == 'sqlsrv') {
+            // this is needed because we need to drop the constraint SQL created when renaming the column (?)
+            $this->dropColumnConstraints('{{%user}}','registration_ip');
+        }
         $this->alterColumn('{{%user}}', 'registration_ip', $this->bigInteger());
     }
 
